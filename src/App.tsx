d9bf32528e6fc3e2fe6e53b706ee5981d8197468
Neolau <ulrick.neola@hotmail.fr>
2020-12-14 11:24:18 +0100

    #checklist

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { ChangeEvent, useEffect, useState } from 'react';
 import './App.css';
+import { Input, Divider, List, Button } from 'antd';
+import { CheckCircleTwoTone } from '@ant-design/icons';
 
 function App() {
+
+  const [elementAjoute, setElementAjoute] = useState('')
+  const [liste, setListe] = useState<string[]>([])
+
+  useEffect(() => {
+    setListe(liste)
+  },[liste])
+
+  const onChangeElementList = (event: ChangeEvent<HTMLInputElement>) => {
+    setElementAjoute(event.currentTarget.value)
+
+  }
+
+  const handleClickAjouter = () => {
+    setListe([...liste, elementAjoute])
+  }
+
+  const handleClickEffacer = () => {
+    setListe([])
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{display: "flex"}}>
+        <Input size="large" placeholder="Ajouter à la liste" onChange={onChangeElementList}/>
+        <Button size="large" type="primary" onClick={handleClickAjouter}>Ajouter</Button>
+      </div>
+      <List
+        size="large"
+        header={<div>Ma liste</div>}
+        bordered
+        dataSource={liste}
+        renderItem={item => (
+          <List.Item>
+            <span>{item} <CheckCircleTwoTone twoToneColor="#52c41a" /></span>
+          </List.Item>
+        )}
+      />
+      <Divider orientation="center"/>
+      <Button size="large" type="primary" onClick={handleClickEffacer}>Effacer la liste</Button>
+      <Divider orientation="center"/>
     </div>
   );
 }
